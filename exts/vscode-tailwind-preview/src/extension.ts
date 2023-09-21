faaa7288eea45786aae75522231edcd5265b7de0
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-09-22 01:45:27 +0900

    feat: Implement vscode extension for previewing tailwind element from IDE (#7)
    Currently, it does not support CSS styling, but I think it's better to split the PR.

---
+++ exts/vscode-tailwind-preview/src/extension.ts
@@ -0,0 +1,69 @@
+import * as vscode from "vscode";
+import * as path from "path";
+import { findMatchingTag, getTagForPosition } from "./tokenizer/tagMatcher";
+import { parseTags } from "./tokenizer/tagParser";
+
+const cats = {
+  "Coding Cat": "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif",
+  "Compiling Cat": "https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif",
+  "Testing Cat": "https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif",
+};
+
+/// Check if the current text looks like a tailwind component.
+function renderHtml(
+  document: vscode.TextDocument,
+  position: vscode.Position
+): [string, vscode.Range] | undefined {
+  if ("jest-snapshot" !== document.languageId) {
+    return undefined;
+  }
+
+  const text = document.getText();
+  const tags = parseTags(text);
+
+  const tag = getTagForPosition(tags, document.offsetAt(position), true);
+
+  if (tag) {
+    const range = new vscode.Range(
+      document.positionAt(tag.opening.start),
+      document.positionAt(tag.closing.end)
+    );
+    return [document.getText(range), range];
+  }
+  return;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  // Show image on hover
+  const hoverProvider: vscode.HoverProvider = {
+    async provideHover(document, position) {
+      const rendeded = renderHtml(document, position);
+      if (!rendeded) {
+        return;
+      }
+      const [htmlText, range] = rendeded;
+
+      const content = new vscode.MarkdownString(htmlText);
+
+      content.supportHtml = true;
+
+      content.isTrusted = true;
+
+      content.supportThemeIcons = true; // to supports codicons
+
+      // baseUri was necessary, full path in the img src did not work
+      // with your icons stroed in the 'images' directory
+      content.baseUri = vscode.Uri.file(
+        path.join(context.extensionPath, "images", path.sep)
+      );
+
+      return new vscode.Hover(content, new vscode.Range(position, position));
+    },
+  };
+
+  for (const lang of ["jest-snapshot"]) {
+    context.subscriptions.push(
+      vscode.languages.registerHoverProvider(lang, hoverProvider)
+    );
+  }
+}
