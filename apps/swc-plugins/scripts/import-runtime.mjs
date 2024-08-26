7427e683ca957987e197093cb480c4479fcf40e1
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-26 16:32:47 +0900

    args

--- apps/swc-plugins/scripts/import-runtime.mjs
+++ apps/swc-plugins/scripts/import-runtime.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env zx
 //
 // Run this script using an absolute path to the runtime directory.
+import path from "path";
 import toml from "toml";
 import { $ } from "zx";
 
@@ -10,14 +11,33 @@ if (!runtimeDir) {
   console.error("Runtime directory is required");
   process.exit(1);
 }
+const $$ = $({ cwd: runtimeDir });
+
+const repositoryRoot = (await $$`git rev-parse --show-toplevel`.text()).trim();
+const cargoLockPath = path.resolve(`${runtimeDir}/Cargo.lock`);
+const relativePathToCargoLock = path.relative(repositoryRoot, cargoLockPath);
+
+console.log("Runtime dir:", runtimeDir);
+console.log("Repository root:", repositoryRoot);
+console.log("Cargo.lock path:", cargoLockPath);
+console.log("Relative path to Cargo.lock:", relativePathToCargoLock);
+
+if (!relativePathToCargoLock.startsWith("..")) {
+  console.error(
+    "Runtime directory is not a subdirectory of the repository root"
+  );
+  process.exit(1);
+}
 
 // Get all git tags
-const gitTags = await $`git tag`.text();
+
+const gitTags = (await $$`git tag`.text()).split("\n");
 
 // For each tag, get the content of `${runtimeDir}/Cargo.lock`.
 for (const tag of gitTags) {
   try {
-    const cargoLock = await $`git show ${tag}:${runtimeDir}/Cargo.lock`.text();
+    const cargoLock =
+      await $$`git show ${tag}:${relativePathToCargoLock}`.text();
     console.log(cargoLock);
 
     const parsed = toml.parse(cargoLock);
