b18ba155e9e4cf559cec3e058474bf40df451ae5
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-26 16:17:28 +0900

    import-runtime.sh

--- apps/swc-plugins/scripts/import-runtime.mjs
+++ apps/swc-plugins/scripts/import-runtime.mjs
@@ -1,6 +1,7 @@
-#!/usr/bin/env node
+#!/usr/bin/env zx
 //
 // Run this script using an absolute path to the runtime directory.
+import { $ } from "zx";
 
 const runtimeDir = process.argv[2];
 
@@ -8,3 +9,7 @@ if (!runtimeDir) {
   console.error("Runtime directory is required");
   process.exit(1);
 }
+
+// Get all git tags.
+
+const gitTags = await $`git tag`.text();
---
+++ apps/swc-plugins/scripts/import-runtime.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+deno run --allow-read --allow-sys --allow-env --allow-run ./scripts/import-runtime.mjs $@
