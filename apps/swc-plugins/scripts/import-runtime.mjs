eca00b340b5508bf3ede759aa03c77e94bb815f8
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-26 16:23:05 +0900

    Remove deno

--- apps/swc-plugins/scripts/import-runtime.mjs
+++ apps/swc-plugins/scripts/import-runtime.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env zx
 //
 // Run this script using an absolute path to the runtime directory.
+import toml from "toml";
 import { $ } from "zx";
 
 const runtimeDir = process.argv[2];
@@ -10,6 +11,17 @@ if (!runtimeDir) {
   process.exit(1);
 }
 
-// Get all git tags.
-
+// Get all git tags
 const gitTags = await $`git tag`.text();
+
+// For each tag, get the content of `${runtimeDir}/Cargo.lock`.
+for (const tag of gitTags) {
+  try {
+    const cargoLock = await $`git show ${tag}:${runtimeDir}/Cargo.lock`.text();
+    console.log(cargoLock);
+
+    const parsed = toml.parse(cargoLock);
+  } catch (e) {
+    console.error(`Failed to parse Cargo.lock for tag ${tag}: ${e}`);
+  }
+}
--- apps/swc-plugins/scripts/import-runtime.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-deno run --allow-read --allow-sys --allow-env --allow-run ./scripts/import-runtime.mjs $@
