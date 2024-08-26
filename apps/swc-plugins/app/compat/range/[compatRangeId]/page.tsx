d71a13d9948b67d151eaff74c6daf8328c98a767
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-26 14:37:00 +0900

    show data

--- apps/swc-plugins/app/compat/range/[compatRangeId]/page.tsx
+++ apps/swc-plugins/app/compat/range/[compatRangeId]/page.tsx
@@ -17,6 +17,25 @@ export default function Page({
         <kbd>swc_core</kbd>@<kbd>{compatRange.from}</kbd> -{" "}
         <kbd>{compatRange.to}</kbd>
       </h1>
+
+      <h2>Runtimes</h2>
+      <ul>
+        {compatRange.runtimes.map((runtime) => (
+          <li key={runtime.name}>
+            <kbd>{runtime.name}</kbd>
+            <kbd>{runtime.minVersion}</kbd> - <kbd>{runtime.maxVersion}</kbd>
+          </li>
+        ))}
+      </ul>
+
+      <h2>Plugins</h2>
+      <ul>
+        {compatRange.plugins.map((plugin) => (
+          <li key={plugin.name}>
+            <kbd>{plugin.name}</kbd>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
