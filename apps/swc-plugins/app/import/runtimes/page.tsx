abc502195ccf064d26c7c6a0175e18eee4d22412
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-26 15:53:31 +0900

    git

--- apps/swc-plugins/app/import/runtimes/page.tsx
+++ apps/swc-plugins/app/import/runtimes/page.tsx
@@ -1,13 +1,11 @@
-import fs from "node:fs/promises";
-
 export default async function Page() {
   if (process.env.NODE_ENV === "production") {
     return <div>Not allowed</div>;
   }
 
-  const plugins = JSON.parse(
-    await fs.readFile("./data/.cache/plugins.json", "utf8")
-  );
+  for (const runtime of Object.keys(runtimes)) {
+    const git = runtimes[runtime].git;
+  }
 }
 
 export const dynamic = "force-dynamic";
