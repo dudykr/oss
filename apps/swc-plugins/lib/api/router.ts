d71e94e789fa3f35812472075c67c90af82548af
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-26 13:35:15 +0900

    fix

--- apps/swc-plugins/lib/api/router.ts
+++ apps/swc-plugins/lib/api/router.ts
@@ -1,8 +1,12 @@
-import { router } from "@/lib/base";
+import { publicProcedure, router } from "@/lib/base";
 
 import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+import { z } from "zod";
 
 export const apiRouter = router({
+  hello: publicProcedure.input(z.string()).query(({ input }) => {
+    return `Hello ${input}`;
+  }),
 });
 
 export type ApiRouter = typeof apiRouter;
