e5a4c6d4fcb5630dddf468cd2259340024f339e4
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-08-26 13:55:04 +0900

    feat: Prepare SWC plugin registry (#41)

--- packages/next-image-snapshot/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type import('jest').Config */
-module.exports = {
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  transform: {
-    "^.+\\.(t|j)sx?$": "@swc/jest",
-  },
-  testTimeout: 90000,
-};
