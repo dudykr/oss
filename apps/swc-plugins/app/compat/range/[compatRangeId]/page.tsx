41e12289afedfe6b96e19bafdfffc31341c63188
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-26 14:36:27 +0900

    range

--- apps/swc-plugins/app/compat/range/[compatRangeId]/page.tsx
+++ apps/swc-plugins/app/compat/range/[compatRangeId]/page.tsx
@@ -1,9 +1,22 @@
 "use client";
 
+import { apiClient } from "@/lib/trpc/web-client";
+
 export default function Page({
-  params: { version },
+  params: { compatRangeId },
 }: {
-  params: { version: string };
+  params: { compatRangeId: string };
 }) {
-  return <div>Hello</div>;
+  const [compatRange] = apiClient.compatRange.get.useSuspenseQuery({
+    id: BigInt(compatRangeId),
+  });
+
+  return (
+    <div>
+      <h1>
+        <kbd>swc_core</kbd>@<kbd>{compatRange.from}</kbd> -{" "}
+        <kbd>{compatRange.to}</kbd>
+      </h1>
+    </div>
+  );
 }
