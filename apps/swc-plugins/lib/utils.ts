5e4986340c335a51bc63c681c25c5531653fac6e
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-26 13:26:45 +0900

    shadci ui

--- apps/swc-plugins/lib/utils.ts
+++ apps/swc-plugins/lib/utils.ts
@@ -1,11 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
-import { Route } from "next";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function route<T extends string>(t: Route<T>): Route {
-  return t as Route;
+  return twMerge(clsx(inputs))
 }
