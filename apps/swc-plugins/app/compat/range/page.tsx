9a68118fc4556238b106dd9d08581c78332b856a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-26 14:37:47 +0900

    ranges

--- apps/swc-plugins/app/compat/range/page.tsx
+++ apps/swc-plugins/app/compat/range/page.tsx
@@ -1,5 +1,24 @@
 "use client";
 
+import { apiClient } from "@/lib/trpc/web-client";
+import Link from "next/link";
+
 export default function Page() {
-  return <div>Hello</div>;
+  const [ranges] = apiClient.compatRange.list.useSuspenseQuery();
+
+  return (
+    <div>
+      <h1>Compat Ranges</h1>
+      <ul>
+        {ranges.map((range) => (
+          <li key={range.id}>
+            <Link href={`/compat/range/${range.id}`}>
+              <kbd>swc_core</kbd>@<kbd>{range.from}</kbd> -{" "}
+              <kbd>{range.to}</kbd>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
