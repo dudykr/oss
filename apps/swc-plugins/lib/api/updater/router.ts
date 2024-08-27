ed5535445b55e350c4202dcbb3f4574796a97f6b
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-28 06:11:27 +0900

    Add endpoint

--- apps/swc-plugins/lib/api/updater/router.ts
+++ apps/swc-plugins/lib/api/updater/router.ts
@@ -14,19 +14,24 @@ function validateToken(token: string) {
   });
 }
 
-const NpmPackageVersionSchema = z.object({
+const PackageVersionSchema = z.object({
   version: z.string(),
   swcCoreVersion: z.string(),
 });
 
-const NpmPackageSchema = z.object({
+const PackageSchema = z.object({
   name: z.string(),
-  versions: z.array(NpmPackageVersionSchema),
+  versions: z.array(PackageVersionSchema),
 });
 
 export const UpdateWasmPluginsInputSchema = z.object({
   token: z.string(),
-  pkgs: z.array(NpmPackageSchema),
+  pkgs: z.array(PackageSchema),
+});
+
+export const UpdateRuntimesInputSchema = z.object({
+  token: z.string(),
+  pkgs: z.array(PackageSchema),
 });
 
 export const updaterRouter = router({
@@ -71,6 +76,60 @@ export const updaterRouter = router({
             },
             create: {
               pluginId: plugin.id,
+              version: version.version,
+              compatRangeId: compatRange.id,
+              swcCoreVersion,
+            },
+            update: {
+              compatRangeId: compatRange.id,
+              swcCoreVersion,
+            },
+          });
+        }
+      }
+    }),
+
+  updateRuntimes: publicProcedure
+    .input(UpdateRuntimesInputSchema)
+    .output(z.void())
+    .mutation(async ({ input, ctx }) => {
+      validateToken(input.token);
+
+      const api = await (await import("@/lib/api/server")).createCaller(ctx);
+
+      for (const pkg of input.pkgs) {
+        const runtime = await db.swcRuntime.upsert({
+          where: {
+            name: pkg.name,
+          },
+          create: {
+            name: pkg.name,
+          },
+          update: {},
+        });
+
+        for (const version of pkg.versions) {
+          const swcCoreVersion = version.swcCoreVersion;
+          const compatRange = await api.compatRange.byCoreVersion({
+            version: swcCoreVersion,
+          });
+
+          if (!compatRange) {
+            throw new TRPCError({
+              code: "NOT_FOUND",
+              message: `Compat range not found for SWC core version ${swcCoreVersion}`,
+            });
+          }
+
+          await db.swcRuntimeVersion.upsert({
+            where: {
+              runtimeId_version: {
+                runtimeId: runtime.id,
+                version: version.version,
+              },
+            },
+            create: {
+              runtimeId: runtime.id,
               version: version.version,
               compatRangeId: compatRange.id,
               swcCoreVersion,
