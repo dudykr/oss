ab9ed75d6ab3be7e28f7d783e004225e9d1a3ca4
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-27 12:31:05 +0900

    addCacheForCrates

--- apps/swc-plugins/lib/api/compatRange/router.ts
+++ apps/swc-plugins/lib/api/compatRange/router.ts
@@ -132,6 +132,85 @@ export const compatRangeRouter = router({
 
       return null;
     }),
+
+  addCacheForCrates: publicProcedure
+    .input(
+      z.object({
+        pluginRunnerVersions: z.array(z.string()),
+        coreVersions: z.array(
+          z.object({
+            version: z.string().describe("The version of the swc_core"),
+            pluginRunnerReq: z.string(),
+          })
+        ),
+      })
+    )
+    .output(z.void())
+    .mutation(
+      async ({ ctx, input: { coreVersions, pluginRunnerVersions } }) => {
+        const compatRanges = await db.compatRange.findMany({
+          select: {
+            id: true,
+            from: true,
+            to: true,
+          },
+        });
+
+        function byVersion(swcCoreVersion: string) {
+          for (const range of compatRanges) {
+            if (
+              semver.gte(swcCoreVersion, range.from) &&
+              (range.to === "*" || semver.lte(swcCoreVersion, range.to))
+            ) {
+              return range;
+            }
+          }
+        }
+
+        for (const corePkg of coreVersions) {
+          corePkg.version = corePkg.version.replace("v", "");
+
+          const compatRange = byVersion(corePkg.version);
+
+          if (!compatRange) {
+            console.error(`Compat range not found for ${corePkg.version}`);
+            continue;
+          }
+
+          for (let rv of pluginRunnerVersions) {
+            rv = rv.replace("v", "");
+
+            if (semver.satisfies(rv, corePkg.pluginRunnerReq)) {
+              await db.swcPluginRunnerVersion.upsert({
+                where: {
+                  version: rv,
+                },
+                create: {
+                  version: rv,
+                  compatRangeId: compatRange.id,
+                },
+                update: {},
+              });
+            }
+          }
+
+          await db.swcCoreVersion.upsert({
+            where: {
+              version: corePkg.version,
+            },
+            create: {
+              version: corePkg.version,
+              pluginRunnerReq: corePkg.pluginRunnerReq,
+              compatRangeId: compatRange.id,
+            },
+            update: {
+              pluginRunnerReq: corePkg.pluginRunnerReq,
+            },
+          });
+          console.log(`Imported ${corePkg.version}`);
+        }
+      }
+    ),
 });
 
 function merge(ranges: { name: string; version: string }[]): VersionRange[] {
