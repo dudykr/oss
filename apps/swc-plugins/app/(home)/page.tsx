0880e80c301e06ddd754e73885d20b3b6b5d4123
Hayden Bleasel <hello@haydenbleasel.com>
2024-08-28 12:38:48 -0400

    Update page.tsx

--- apps/swc-plugins/app/(home)/page.tsx
+++ apps/swc-plugins/app/(home)/page.tsx
@@ -28,7 +28,8 @@ const Home: FC = () => {
 
   return (
     <div className="flex flex-col items-center gap-4">
-      <div className="flex items-center gap-4">
+      <h1 className="text-2xl font-bold">SWC Plugins</h1>
+      <div className="flex items-center gap-2">
         <Select
           value={selectedRuntime?.toString()}
           data={runtimes.map((runtime) => ({
